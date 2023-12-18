860939d3654bd5ee2aa0be5152b0c13c262de8b4
IHiuHiu <88642933+IHiuHiu@users.noreply.github.com>
2023-12-18 16:07:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -83,6 +83,9 @@ def infer(text, vectorizer, model):
 
     print(f'Label: {y_pred[0]}')
 
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
 for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
